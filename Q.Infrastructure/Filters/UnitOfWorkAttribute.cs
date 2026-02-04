cdcacc1bd5190453838d9b28c410d1f41861eb6e
qw <qw@123.com>
2026-02-04 15:13:32 +0800

    added uowfilter

---
+++ Q.Infrastructure/Filters/UnitOfWorkAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Q.Infrastructure.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class UnitOfWorkAttribute : Attribute
+    {
+        public Type[] DbContextTypes { get; init; }
+
+        public UnitOfWorkAttribute(params Type[] dbContextTypes)
+        {
+            DbContextTypes = dbContextTypes;
+
+            foreach (var dbContextType in dbContextTypes)
+            {
+                if (!typeof(DbContext).IsAssignableFrom(dbContextType))
+                {
+                    throw new ArgumentException($"Type {dbContextType.FullName} is not a DbContext");
+                }
+            }
+        }
+    }
+}
---
+++ Q.Infrastructure/Filters/UnitOfWorkFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Transactions;
+
+namespace Q.Infrastructure.Filters
+{
+    public class UnitOfWorkFilter : IAsyncActionFilter
+    {
+        public static UnitOfWorkAttribute? GetUnitOfWorkAttribute(ActionDescriptor actionDescriptor)
+        {
+            if (actionDescriptor is not ControllerActionDescriptor actionContext) return null;
+            var uowAttribute = actionContext.ControllerTypeInfo.GetCustomAttribute<UnitOfWorkAttribute>()
+                ?? actionContext.MethodInfo.GetCustomAttribute<UnitOfWorkAttribute>();
+            return uowAttribute;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var result = await next();
+            if (result.Exception is not null || context.HttpContext.Response.StatusCode >= 400) return;
+            var uowAttribute = GetUnitOfWorkAttribute(context.ActionDescriptor);
+            if (uowAttribute == null || uowAttribute.DbContextTypes.Length == 0) return;
+            using TransactionScope txScope = new(TransactionScopeAsyncFlowOption.Enabled);
+            // ToList的简化写法
+            List<DbContext?> dbContextList = [.. uowAttribute.DbContextTypes.Select(t => context.HttpContext.RequestServices.GetService(t) as DbContext)];
+            foreach (var dbContext in dbContextList)
+            {
+                if (dbContext is not null)
+                    await dbContext.SaveChangesAsync();
+            }
+            txScope.Complete();
+        }
+    }
+}
